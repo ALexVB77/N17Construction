5ac4e39a4f9c1e957db411e246a37563ebd1ebb4
George Garin <ggarin@navicons.ru>
2021-05-21 18:26:20 +0300

    fix

--- Scripts/Translater/TranslateFromBase.ps1
+++ Scripts/Translater/TranslateFromBase.ps1
@@ -1,15 +1,8 @@
-﻿
-$srcFile = "..\..\AL\BaseApp\Translations\Base Application.ru-RU.xlf"
+﻿$srcFile = "..\..\AL\BaseApp\Translations\Base Application.ru-RU.xlf"
 $dstFile = "..\..\AL\GeneralExt\Translations\GeneralExt.ru-RU.xlf"
-
-#Test-Path -Path $srcFile -PathType Leaf
-#Test-Path -Path $dstFile -PathType Leaf
-
-
 
 [xml]$src =  Get-Content -Path $srcFile -Encoding UTF8
 [xml]$dst =  Get-Content -Path $dstFile -Encoding UTF8
-
 
 $dstUnits = $dst.xliff.file.body.group.'trans-unit' | Where-Object {$_.target -eq '[NAB: NOT TRANSLATED]'}
 foreach($dstNode in $dstUnits){
