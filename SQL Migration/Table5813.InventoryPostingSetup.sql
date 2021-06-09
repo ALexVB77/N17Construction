d2c6c64a966e7304c7b114154a01aa1b6cb5f3be
AlexeyPopov1997 <popov.alexey1997@gmail.com>
2021-06-09 16:49:20 +0200

    Updated script

--- SQL Migration/Table5813.InventoryPostingSetup.sql
+++ SQL Migration/Table5813.InventoryPostingSetup.sql
@@ -1,7 +1,4 @@
-USE [Bonava Objects]
-GO
-
-INSERT INTO [Bonava-Dev].[dbo].[Bonava$Inventory Posting Setup$437dbf0e-84ff-417a-965d-ed2bb9650972]
+INSERT INTO [Bonava-Test].[dbo].[Bonava$Inventory Posting Setup$437dbf0e-84ff-417a-965d-ed2bb9650972]
 (
 	[Location Code],
 	[Invt_ Posting Group Code],
@@ -9,15 +6,11 @@ INSERT INTO [Bonava-Dev].[dbo].[Bonava$Inventory Posting Setup$437dbf0e-84ff-417
 )
 SELECT
 	[Location Code],
-	(SELECT GLAccMapping.[New No_]
-	 FROM [dbo].[test$Inventory Posting Setup]
-	 INNER JOIN [Bonava-Dev].[dbo].[Bonava$G_L Account Mapping$2944687f-9cf8-4134-a24c-e21fb70a8b1a] GLAccMapping
-	 ON GLAccMapping.[Old No_] = [Invt_ Posting Group Code]),
-	(SELECT GLAccMapping.[New No_]
-	 FROM [dbo].[test$Inventory Posting Setup]
-	 INNER JOIN [Bonava-Dev].[dbo].[Bonava$G_L Account Mapping$2944687f-9cf8-4134-a24c-e21fb70a8b1a] GLAccMapping
-	 ON GLAccMapping.[Old No_] = [Inventory Account])
-FROM [dbo].[test$Inventory Posting Setup]
-INNER JOIN [Bonava-Dev].[dbo].[Bonava$Location$2944687f-9cf8-4134-a24c-e21fb70a8b1a] Location
-ON Location.[Code] = [Location Code]
+	GLAccMapping.[New No_] AS [Invt_ Posting Group Code],
+	GLAccMapping.[New No_] AS [Inventory Account]
+FROM [VM-PRO-SQL007\NAV].[NAV_for_Developers].[dbo].[Bonava$Inventory Posting Setup]
+INNER JOIN [Bonava-Test].[dbo].[Bonava$G_L Account Mapping$2944687f-9cf8-4134-a24c-e21fb70a8b1a] GLAccMapping
+ON GLAccMapping.[Old No_] = [Invt_ Posting Group Code] collate Cyrillic_General_100_CI_AS
+INNER JOIN [Bonava-Test].[dbo].[Bonava$Location$2944687f-9cf8-4134-a24c-e21fb70a8b1a] Location
+ON Location.[Code] = [Location Code] collate Cyrillic_General_100_CI_AS
 WHERE Location.Blocked = 0;
