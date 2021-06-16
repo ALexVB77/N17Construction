dbc36f662b1a81dcd695c1056a0028fd0ae09f5b
AlexeyPopov1997 <popov.alexey1997@gmail.com>
2021-06-17 01:46:36 +0300

    Added new scripts

---
+++ SQL Migration/Table3.PaymentTerms.sql
@@ -0,0 +1,15 @@
+INSERT INTO [Bonava-Test].[dbo].[Bonava$Payment Terms$437dbf0e-84ff-417a-965d-ed2bb9650972]
+(
+	[Code],
+	[Due Date Calculation],
+	[Discount Date Calculation],
+	[Discount _],
+	[Description]
+)
+SELECT
+	[Code],
+	[Due Date Calculation],
+	[Discount Date Calculation],
+	[Discount %],
+	[Description]
+FROM [VM-PRO-SQL007\NAV].[NAV_for_Developers].[dbo].[Bonava$Payment Terms]
