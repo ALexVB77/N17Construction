b24fac445888ba48766cd57c93eb82b19a730988
AlexeyPopov1997 <popov.alexey1997@gmail.com>
2021-06-28 23:16:05 +0300

    Update scripts

--- Scripts/RepublishBaseAppTest.ps1
+++ Scripts/RepublishBaseAppTest.ps1
@@ -1,11 +1,5 @@
-# cd "C:\Distr\Scripts"
-
-# $BankStatementPath = "C:\Users\alpopov\AL\N17Construction\AL\BankStatement\Default publisher_Bank Statement_1.0.0.0.app"
-# $ExcelBufferModExtPath = "C:\Users\alpopov\AL\N17Construction\AL\ExcelBufferModExt\Default publisher_ExcelBufferModExt_1.0.0.0.app"
-# $GeneralExtPath = "C:\Users\alpopov\AL\N17Construction\AL\GeneralExt\Default publisher_GeneralExt_1.0.0.5.app"
-
 $ServerInstance = "BonavaTest"
-$ApplicationPath = "C:\Users\ZEXT.rualepop\AL\N17Construction\AL\BaseApp\Microsoft_Base Application_17.4.21491.21534.app"
+$ApplicationPath = "C:\Users\ZEXT.rualepop\AL\N17Construction\AL\BaseApp\Microsoft_Base Application_17.4.21491.21535.app"
 
 $quietExecution = $null
 
@@ -13,9 +7,5 @@ $quietExecution = ./Modules/NavAdminTool.ps1
 $quietExecution = Import-Module ".\Modules\AppAdminToolTest.psm1"
 
 RepublishBaseApp -ServerInstanceName $ServerInstance -ApplicationName "Base Application" -ApplicationPath $ApplicationPath
-
-# PublishAndInstall -ServerInstance $ServerInstance -AppName "Bank Statement" -AppPath $BankStatementPath
-# PublishAndInstall -ServerInstance $ServerInstance -AppName "ExcelBufferModExt" -AppPath $ExcelBufferModExtPath 
-# PublishAndInstall -ServerInstance $ServerInstance -AppName "GeneralExt" -AppPath $GeneralExtPath 
 
 Write-Output "Base Application has been republished successfully."
