97e755354d615eff94a193dd7787bf04d76442f2
AlexeyPopov1997 <popov.alexey1997@gmail.com>
2021-07-02 17:24:52 +0300

    Update scripts

--- SQL Migration/Bonava/Table23.Vendor [Bonava].sql
+++ SQL Migration/Bonava/Table23.Vendor [Bonava].sql
@@ -94,3 +94,52 @@ FROM [VM-PRO-SQL007\NAV].[NAV_for_Developers].[dbo].[Bonava$Vendor]
 LEFT JOIN [Bonava-Test].[dbo].[Bonava$G_L Account Mapping$2944687f-9cf8-4134-a24c-e21fb70a8b1a] GLAccMapping
 ON GLAccMapping.[Old No_] = [Vat Agent Posting Group] collate Cyrillic_General_100_CI_AS
 WHERE [Blocked] <> '2';
+
+--Default Dimension
+INSERT INTO [Bonava-Test].[dbo].[Bonava$Default Dimension$437dbf0e-84ff-417a-965d-ed2bb9650972]
+(
+	[Table ID],
+	[No_],
+	[Dimension Code],
+	[Dimension Value Code],
+	[Value Posting],
+	[Multi Selection Action]
+)
+SELECT
+	DefaultDimension.[Table ID],
+	DefaultDimension.[No_],
+	DefaultDimension.[Dimension Code],
+	DefaultDimension.[Dimension Value Code],
+	DefaultDimension.[Value Posting],
+	DefaultDimension.[Multi Selection Action]
+FROM [VM-PRO-SQL007\NAV].[NAV_for_Developers].[dbo].[Bonava$Default Dimension] AS DefaultDimension
+LEFT JOIN [Bonava-Test].[dbo].[Bonava$Dimension Value$437dbf0e-84ff-417a-965d-ed2bb9650972] DimensionValue
+ON DimensionValue.[Code] = DefaultDimension.[Dimension Value Code] collate Cyrillic_General_100_CI_AS
+WHERE (DefaultDimension.[Dimension Code] = 'CC' OR 
+	   DefaultDimension.[Dimension Code] = 'НП' OR
+	   DefaultDimension.[Dimension Code] = 'НУ-ВИД' OR
+	   DefaultDimension.[Dimension Code] = 'НУ-ОБЪЕКТ' OR
+	   DefaultDimension.[Dimension Code] = 'НУ-РАЗНИЦА' OR
+	   DefaultDimension.[Dimension Code] = 'ПРИБ_УБ_ПРОШ_ЛЕТ')
+AND DefaultDimension.[Table ID] = '23';
+
+INSERT INTO [Bonava-Test].[dbo].[Bonava$Default Dimension$437dbf0e-84ff-417a-965d-ed2bb9650972]
+(
+	[Table ID],
+	[No_],
+	[Dimension Code],
+	[Dimension Value Code],
+	[Value Posting],
+	[Multi Selection Action]
+)
+SELECT
+	DefaultDimension.[Table ID],
+	DefaultDimension.[No_],
+	DefaultDimension.[Dimension Code],
+	ISNULL(DimensionMapping.[New Dimension Value Code], '') AS [Dimension Value Code],
+	DefaultDimension.[Value Posting],
+	DefaultDimension.[Multi Selection Action]
+FROM [VM-PRO-SQL007\NAV].[NAV_for_Developers].[dbo].[Bonava$Default Dimension] AS DefaultDimension
+LEFT JOIN [Bonava-Test].[dbo].[Bonava$Dimension Mapping$2944687f-9cf8-4134-a24c-e21fb70a8b1a] DimensionMapping
+ON DimensionMapping.[Old Dimension Value Code] = DefaultDimension.[Dimension Value Code] collate Cyrillic_General_100_CI_AS
+WHERE DefaultDimension.[Dimension Code] = 'CP' AND DefaultDimension.[Table ID] = '23';
