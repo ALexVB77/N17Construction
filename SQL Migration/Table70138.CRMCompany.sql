baaa7f6326996b2baaf82758bcc5704c8a988877
AlexeyPopov1997 <popov.alexey1997@gmail.com>
2021-07-15 16:46:35 +0300

    Updated script

--- SQL Migration/Table70138.CRMCompany.sql
+++ SQL Migration/Table70138.CRMCompany.sql
@@ -10,5 +10,5 @@ INSERT INTO [Bonava-Test].[dbo].[CRM Company$2944687f-9cf8-4134-a24c-e21fb70a8b1
 SELECT
 	[Project Guid],
 	[Project Name],
-	[NCC Company]
+	CASE WHEN [NCC Company] = 'NCC Real Estate' THEN 'Real Estate' ELSE [NCC Company] END AS [NCC Company]
 FROM [VM-PRO-SQL007\NAV].[NAV_for_Developers].[dbo].[NavCRM Companies];
