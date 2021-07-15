9d3524f960ca7c94e089204a4227de4eb8ab01cb
AlexeyPopov1997 <popov.alexey1997@gmail.com>
2021-07-15 16:02:00 +0300

    Added script

---
+++ SQL Migration/Table70138.CRMCompany.sql
@@ -0,0 +1,14 @@
+DELETE FROM [Bonava-Test].[dbo].[CRM Company$2944687f-9cf8-4134-a24c-e21fb70a8b1a];
+
+-- CRM Company
+INSERT INTO [Bonava-Test].[dbo].[CRM Company$2944687f-9cf8-4134-a24c-e21fb70a8b1a]
+(
+	[Project Guid],
+	[Project Name],
+	[Company Name]
+)
+SELECT
+	[Project Guid],
+	[Project Name],
+	[NCC Company]
+FROM [VM-PRO-SQL007\NAV].[NAV_for_Developers].[dbo].[NavCRM Companies];
